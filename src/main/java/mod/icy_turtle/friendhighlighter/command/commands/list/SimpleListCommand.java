461ca665dbb276a5527891fbcbd19c7400cb83d4
Incandescent-Turtle <rorsmacd@gmail.com>
2024-01-03 18:50:36 -0800

    added toggle to simple list
    - simple list can toggle the highlighter on and off now

--- src/main/java/mod/icy_turtle/friendhighlighter/command/commands/list/SimpleListCommand.java
+++ src/main/java/mod/icy_turtle/friendhighlighter/command/commands/list/SimpleListCommand.java
@@ -1,9 +1,12 @@
 package mod.icy_turtle.friendhighlighter.command.commands.list;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import mod.icy_turtle.friendhighlighter.FriendHighlighter;
 import mod.icy_turtle.friendhighlighter.command.Command;
 import mod.icy_turtle.friendhighlighter.command.CommandHandler;
+import mod.icy_turtle.friendhighlighter.command.CommandUtils;
 import mod.icy_turtle.friendhighlighter.config.FriendsListHandler;
+import mod.icy_turtle.friendhighlighter.util.FHUtils;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
@@ -26,7 +29,10 @@ public class SimpleListCommand extends Command
 
 	public static MutableText createSimpleList()
 	{
-		MutableText txt = Text.literal("").append("Friends List: ");
+		MutableText txt = Text.literal("").append(CommandUtils.addHoverAndClickEvent(
+				FHUtils.getMessageWithConnotation("Friends List", FriendHighlighter.isHighlighterEnabled),
+				"Click to toggle Friend Highlighter",
+				"/fh toggle"));
 		var map = FriendsListHandler.getFriendsMap();
 		if(map.size() == 0)
 			return txt.append(" Empty.");
