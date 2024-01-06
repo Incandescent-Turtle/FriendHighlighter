eca936a94bfd4397d611575e8c7c6c3216048df0
Incandescent-Turtle <rorsmacd@gmail.com>
2024-01-06 00:38:04 -0500

    updated dependencies

--- src/main/java/mod/icy_turtle/friendhighlighter/config/ModMenuIntegration.java
+++ src/main/java/mod/icy_turtle/friendhighlighter/config/ModMenuIntegration.java
@@ -2,31 +2,21 @@ package mod.icy_turtle.friendhighlighter.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.util.Utils;
-import me.shedaniel.clothconfig2.api.*;
+import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import me.shedaniel.clothconfig2.gui.entries.MultiElementListEntry;
 import me.shedaniel.clothconfig2.gui.entries.NestedListListEntry;
-import me.shedaniel.clothconfig2.impl.builders.DropdownMenuBuilder;
-import me.shedaniel.clothconfig2.impl.builders.SubCategoryBuilder;
 import mod.icy_turtle.friendhighlighter.FriendHighlighter;
 import mod.icy_turtle.friendhighlighter.util.FHUtils;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.registry.Registries;
-import net.minecraft.text.ClickEvent;
-import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.Util;
 
 import java.util.*;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 /**
  * Integration for the ModMenu mod.
@@ -79,70 +69,70 @@ public class ModMenuIntegration implements ModMenuApi
         }
     }
 
-    private void createThing(ConfigCategory testing, ConfigEntryBuilder entryBuilder)
-    {
-        //SPAWN_EGGS
-        testing.addEntry(entryBuilder.startDropdownMenu(Text.literal("Mob Drop Down"), DropdownMenuBuilder.TopCellElementBuilder.of(new FakeMob("dog"), FakeMob::createFromString, FakeMob::toText), DropdownMenuBuilder.CellCreatorBuilder.of(100,100, 5, FakeMob::toText)).setSelections(Arrays.stream("bob john henry harrison marge lemon".split(" ")).map(s -> new FakeMob(s)).collect(Collectors.toList())).build());
-        testing.addEntry(entryBuilder.startKeyCodeField(Text.literal("Cool Key"), InputUtil.UNKNOWN_KEY).setDefaultValue(InputUtil.UNKNOWN_KEY).build());
-
-        testing.addEntry(entryBuilder.startModifierKeyCodeField(Text.literal("Cool Modifier Key"), ModifierKeyCode.of(InputUtil.Type.KEYSYM.createFromCode(79), Modifier.of(false, true, false))).setDefaultValue(ModifierKeyCode.of(InputUtil.Type.KEYSYM.createFromCode(79), Modifier.of(false, true, false))).build());
-        SubCategoryBuilder colors = entryBuilder.startSubCategory(Text.literal("Colors")).setExpanded(true);
-        colors.add(entryBuilder.startDropdownMenu(Text.literal("lol apple"), DropdownMenuBuilder.TopCellElementBuilder.ofItemObject(Items.APPLE), DropdownMenuBuilder.CellCreatorBuilder.ofItemObject()).setDefaultValue(Items.APPLE).setSelections(Registries.ITEM.stream().sorted(Comparator.comparing(Item::toString)).collect(Collectors.toCollection(LinkedHashSet::new))).setSaveConsumer(item -> System.out.println("save this " + item)).build());
-        SubCategoryBuilder innerColors = entryBuilder.startSubCategory(Text.literal("Inner Colors")).setExpanded(true);
-        innerColors.add(entryBuilder.startDropdownMenu(Text.literal("lol apple"), DropdownMenuBuilder.TopCellElementBuilder.ofItemObject(Items.APPLE), DropdownMenuBuilder.CellCreatorBuilder.ofItemObject()).setDefaultValue(Items.APPLE).setSelections(Registries.ITEM.stream().sorted(Comparator.comparing(Item::toString)).collect(Collectors.toCollection(LinkedHashSet::new))).setSaveConsumer(item -> System.out.println("save this " + item)).build());
-        colors.add(innerColors.build());
-        testing.addEntry(colors.build());
-        testing.addEntry(entryBuilder.startDropdownMenu(Text.literal("Suggestion Random Int"), DropdownMenuBuilder.TopCellElementBuilder.of(10,
-                s -> {
-                    try {
-                        return Integer.parseInt(s);
-                    } catch (NumberFormatException ignored) {
-
-                    }
-                    return null;
-                })).setDefaultValue(10).setSelections(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))).build());
-        testing.addEntry(entryBuilder.startDropdownMenu(Text.literal("Selection Random Int"), DropdownMenuBuilder.TopCellElementBuilder.of(10,
-                s -> {
-                    try {
-                        return Integer.parseInt(s);
-                    } catch (NumberFormatException ignored) {
-
-                    }
-                    return null;
-                })).setDefaultValue(5).setSuggestionMode(false).setSelections(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))).build());
-        testing.addEntry(new NestedListListEntry<Pair<Integer, Integer>, MultiElementListEntry<Pair<Integer, Integer>>>(
-                Text.literal("Nice"),
-                new ArrayList<>(List.of(new Pair<>(10, 10), new Pair<>(20, 40))),
-                false,
-                Optional::empty,
-                list -> {},
-                () -> new ArrayList<>(List.of(new Pair<>(10, 10), new Pair<>(20, 40))),
-                entryBuilder.getResetButtonKey(),
-                true,
-                true,
-                (elem, nestedListListEntry) -> {
-                    if (elem == null) {
-                        Pair<Integer, Integer> newDefaultElemValue = new Pair<>(10, 10);
-                        return new MultiElementListEntry<>(Text.literal("Pair"), newDefaultElemValue,
-                                new ArrayList<>(List.of(entryBuilder.startIntField(Text.literal("Left"), newDefaultElemValue.getLeft()).setDefaultValue(10).build(),
-                                        entryBuilder.startIntField(Text.literal("Right"), newDefaultElemValue.getRight()).setDefaultValue(10).build())),
-                                true);
-                    } else {
-                        return new MultiElementListEntry<>(Text.literal("Pair"), elem,
-                                new ArrayList<>(List.of(entryBuilder.startIntField(Text.literal("Left"), elem.getLeft()).setDefaultValue(10).build(),
-                                        entryBuilder.startIntField(Text.literal("Right"), elem.getRight()).setDefaultValue(10).build())),
-                                true);
-                    }
-                }
-        ));
-        testing.addEntry(entryBuilder.startTextDescription(
-                Text.translatable("text.cloth-config.testing.1",
-                        Text.literal("ClothConfig").styled(s -> s.withBold(true).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM, new HoverEvent.ItemStackContent(Util.make(new ItemStack(Items.PINK_WOOL), stack -> stack.setCustomName(Text.literal("(\u30FB\u2200\u30FB)")).addEnchantment(Enchantments.EFFICIENCY, 10)))))),
-                        Text.translatable("text.cloth-config.testing.2").styled(s -> s.withColor(Formatting.BLUE).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("https://shedaniel.gitbook.io/cloth-config/"))).withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://shedaniel.gitbook.io/cloth-config/"))),
-                        Text.translatable("text.cloth-config.testing.3").styled(s -> s.withColor(Formatting.GREEN).withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, Utils.getConfigFolder().getParent().resolve("options.txt").toString())))
-                )
-        ).build());
-    }
+//    private void createThing(ConfigCategory testing, ConfigEntryBuilder entryBuilder)
+//    {
+//        //SPAWN_EGGS
+//        testing.addEntry(entryBuilder.startDropdownMenu(Text.literal("Mob Drop Down"), DropdownMenuBuilder.TopCellElementBuilder.of(new FakeMob("dog"), FakeMob::createFromString, FakeMob::toText), DropdownMenuBuilder.CellCreatorBuilder.of(100,100, 5, FakeMob::toText)).setSelections(Arrays.stream("bob john henry harrison marge lemon".split(" ")).map(s -> new FakeMob(s)).collect(Collectors.toList())).build());
+//        testing.addEntry(entryBuilder.startKeyCodeField(Text.literal("Cool Key"), InputUtil.UNKNOWN_KEY).setDefaultValue(InputUtil.UNKNOWN_KEY).build());
+//
+//        testing.addEntry(entryBuilder.startModifierKeyCodeField(Text.literal("Cool Modifier Key"), ModifierKeyCode.of(InputUtil.Type.KEYSYM.createFromCode(79), Modifier.of(false, true, false))).setDefaultValue(ModifierKeyCode.of(InputUtil.Type.KEYSYM.createFromCode(79), Modifier.of(false, true, false))).build());
+//        SubCategoryBuilder colors = entryBuilder.startSubCategory(Text.literal("Colors")).setExpanded(true);
+//        colors.add(entryBuilder.startDropdownMenu(Text.literal("lol apple"), DropdownMenuBuilder.TopCellElementBuilder.ofItemObject(Items.APPLE), DropdownMenuBuilder.CellCreatorBuilder.ofItemObject()).setDefaultValue(Items.APPLE).setSelections(Registries.ITEM.stream().sorted(Comparator.comparing(Item::toString)).collect(Collectors.toCollection(LinkedHashSet::new))).setSaveConsumer(item -> System.out.println("save this " + item)).build());
+//        SubCategoryBuilder innerColors = entryBuilder.startSubCategory(Text.literal("Inner Colors")).setExpanded(true);
+//        innerColors.add(entryBuilder.startDropdownMenu(Text.literal("lol apple"), DropdownMenuBuilder.TopCellElementBuilder.ofItemObject(Items.APPLE), DropdownMenuBuilder.CellCreatorBuilder.ofItemObject()).setDefaultValue(Items.APPLE).setSelections(Registries.ITEM.stream().sorted(Comparator.comparing(Item::toString)).collect(Collectors.toCollection(LinkedHashSet::new))).setSaveConsumer(item -> System.out.println("save this " + item)).build());
+//        colors.add(innerColors.build());
+//        testing.addEntry(colors.build());
+//        testing.addEntry(entryBuilder.startDropdownMenu(Text.literal("Suggestion Random Int"), DropdownMenuBuilder.TopCellElementBuilder.of(10,
+//                s -> {
+//                    try {
+//                        return Integer.parseInt(s);
+//                    } catch (NumberFormatException ignored) {
+//
+//                    }
+//                    return null;
+//                })).setDefaultValue(10).setSelections(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))).build());
+//        testing.addEntry(entryBuilder.startDropdownMenu(Text.literal("Selection Random Int"), DropdownMenuBuilder.TopCellElementBuilder.of(10,
+//                s -> {
+//                    try {
+//                        return Integer.parseInt(s);
+//                    } catch (NumberFormatException ignored) {
+//
+//                    }
+//                    return null;
+//                })).setDefaultValue(5).setSuggestionMode(false).setSelections(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))).build());
+//        testing.addEntry(new NestedListListEntry<Pair<Integer, Integer>, MultiElementListEntry<Pair<Integer, Integer>>>(
+//                Text.literal("Nice"),
+//                new ArrayList<>(List.of(new Pair<>(10, 10), new Pair<>(20, 40))),
+//                false,
+//                Optional::empty,
+//                list -> {},
+//                () -> new ArrayList<>(List.of(new Pair<>(10, 10), new Pair<>(20, 40))),
+//                entryBuilder.getResetButtonKey(),
+//                true,
+//                true,
+//                (elem, nestedListListEntry) -> {
+//                    if (elem == null) {
+//                        Pair<Integer, Integer> newDefaultElemValue = new Pair<>(10, 10);
+//                        return new MultiElementListEntry<>(Text.literal("Pair"), newDefaultElemValue,
+//                                new ArrayList<>(List.of(entryBuilder.startIntField(Text.literal("Left"), newDefaultElemValue.getLeft()).setDefaultValue(10).build(),
+//                                        entryBuilder.startIntField(Text.literal("Right"), newDefaultElemValue.getRight()).setDefaultValue(10).build())),
+//                                true);
+//                    } else {
+//                        return new MultiElementListEntry<>(Text.literal("Pair"), elem,
+//                                new ArrayList<>(List.of(entryBuilder.startIntField(Text.literal("Left"), elem.getLeft()).setDefaultValue(10).build(),
+//                                        entryBuilder.startIntField(Text.literal("Right"), elem.getRight()).setDefaultValue(10).build())),
+//                                true);
+//                    }
+//                }
+//        ));
+//        testing.addEntry(entryBuilder.startTextDescription(
+//                Text.translatable("text.cloth-config.testing.1",
+//                        Text.literal("ClothConfig").styled(s -> s.withBold(true).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM, new HoverEvent.ItemStackContent(Util.make(new ItemStack(Items.PINK_WOOL), stack -> stack.setCustomName(Text.literal("(\u30FB\u2200\u30FB)")).addEnchantment(Enchantments.EFFICIENCY, 10)))))),
+//                        Text.translatable("text.cloth-config.testing.2").styled(s -> s.withColor(Formatting.BLUE).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("https://shedaniel.gitbook.io/cloth-config/"))).withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://shedaniel.gitbook.io/cloth-config/"))),
+//                        Text.translatable("text.cloth-config.testing.3").styled(s -> s.withColor(Formatting.GREEN).withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, Utils.getConfigFolder().getParent().resolve("options.txt").toString())))
+//                )
+//        ).build());
+//    }
     /**
      * Create the list entry for configuring the friends list.
      * @param entryBuilder the entry builder.
